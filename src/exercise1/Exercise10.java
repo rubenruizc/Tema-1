21eeda36b01f336e1b134fc3ca18be0ce2dc71e2
rruiz <rruiz@106-26.info2>
2023-09-22 10:16:17 +0200

    Ejercicio 10

---
+++ src/exercise1/Exercise10.java
@@ -0,0 +1,41 @@
+package exercise1;
+
+import java.util.Scanner;
+
+public class Exercise10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Declaración de variables 
+int anioActual, anioNacimiento, edad;
+Scanner sc= new Scanner(System.in);
+
+//Instruccionesç
+
+System.out.println("Escribe el año actual: ");
+
+System.out.println("Escribe el anio cuando naciste:");
+
+anioActual=sc.nextInt();
+
+anioNacimiento=sc.nextInt();
+
+System.out.println("Estamos en el año: " + anioActual);
+
+System.out.println();
+
+System.out.println("Tu anio de nacimiento es: " + anioNacimiento);
+
+System.out.println();
+
+edad= anioActual - anioNacimiento;
+
+System.out.println("Tu edad es: " + edad + " anios");
+
+
+
+
+	}
+
+}
